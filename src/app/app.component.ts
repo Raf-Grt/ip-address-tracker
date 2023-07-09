c7e5875a2ae88dec123dd9351e664d1008498b39
Raf-Grt <rafael.esparon@gmail.com>
2023-07-09 18:31:02 +0200

    base desktop layout released

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ip-address-tracker';
+export class AppComponent implements OnInit {
+  public inputValue: string = '';
+
+  ngOnInit(): void {
+    const map = L.map('map', { zoomControl: false }).setView(
+      [48.8583, 2.2945],
+      16
+    );
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(map);
+  }
+
+  public logData(): void {
+    console.log(this.inputValue);
+    this.inputValue = '';
+  }
 }
